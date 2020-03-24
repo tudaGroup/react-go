db766aa29d59c28ac209b88e2b4a29c0df04d316
shangsuru <helm.henry99@gmail.com>
2020-03-24 09:54:37 +0100

    Fix error messages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Main from './Main';
 import Login from './Login';
 import Register from './Register';
-import PasswordReset from './PasswordReset';
 import GameWindow from './GameWindow';
 import PasswordResetRequest from './PasswordResetRequest';
 import history from '../history';
