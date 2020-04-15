16fe4cc39d3908a314e60d925432f635c6baf7e3
shangsuru <helm.henry99@gmail.com>
2020-04-15 11:42:18 +0200

    Store rating in games and fix graph display

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -22,7 +22,6 @@ const Profile = () => {
 
   useEffect(() => {
     const token = localStorage.getItem('jwt');
-
     // Redirect to login page if user is without token
     if (token === null) {
       history.push('/login');
@@ -38,15 +37,16 @@ const Profile = () => {
         },
       })
       .then((result) => {
-        console.log(result);
-        setUsername(result.data.username);
-        setGivenName(result.data.givenName);
-        setSurName(result.data.surName);
-        setCity(result.data.location);
-        setCountry(result.data.country);
-        setBiography(result.data.biography);
-        setMemberSince(moment(result.data.memberSince).format('LL'));
+        const user = result.data.user;
+        setUsername(user.username);
+        setGivenName(user.givenName);
+        setSurName(user.surName);
+        setCity(user.location);
+        setCountry(user.country);
+        setBiography(user.biography);
+        setMemberSince(moment(user.memberSince).format('LL'));
 
+        // Create graph coordinates for rating graph
         let ratings = [];
         for (let rating of result.data.ratings) {
           ratings.push({ x: new Date(rating.time), y: rating.rating });
@@ -55,7 +55,7 @@ const Profile = () => {
 
         // Fetch games
         api
-          .get(`games/${result.data.username}`, {
+          .get(`games/${user.username}`, {
             headers: {
               Authorization: 'Bearer ' + token,
             },
@@ -69,6 +69,7 @@ const Profile = () => {
       })
       .catch((e) => {
         setUserNotFound(true);
+        console.log(e);
       });
   }, []);
 
--- routes/games.js
+++ routes/games.js
@@ -24,8 +24,8 @@ router.get('/games', auth, async (req, res) => {
   let games = await Game.find({
     $or: [
       { $and: [{ player1: player1 }, { player2: player2 }] },
-      { $and: [{ player1: player2 }, { player2: player1 }] }
-    ]
+      { $and: [{ player1: player2 }, { player2: player1 }] },
+    ],
   }).sort({ _id: -1 });
   res.send(games);
 });
@@ -37,8 +37,8 @@ router.get('/games/active', auth, async (req, res) => {
   let games = await await Game.find({
     $or: [
       { $and: [{ player1: player1 }, { player2: player2 }] },
-      { $and: [{ player1: player2 }, { player2: player1 }] }
-    ]
+      { $and: [{ player1: player2 }, { player2: player1 }] },
+    ],
   }).sort({ _id: -1 });
   let mostRecentGame = games[0];
 
@@ -54,19 +54,23 @@ router.get('/games/:player', auth, async (req, res) => {
   let player = req.params.player;
   console.log(player);
   let games = await Game.find({
-    $or: [{ player1: player }, { player2: player }]
+    $or: [{ player1: player }, { player2: player }],
   }).sort({ _id: -1 });
 
-  console.log('games:')
+  console.log('games:');
   console.log(games);
-  
-  let wins = games.filter(game => {
-    console.log((game.player1 === player && game.player1Won) ||
-    (game.player1 !== player && !game.player1Won));
-    return (game.player1 === player && game.player1Won) ||
-      (game.player1 !== player && !game.player1Won);
+
+  let wins = games.filter((game) => {
+    console.log(
+      (game.player1 === player && game.player1Won) ||
+        (game.player1 !== player && !game.player1Won)
+    );
+    return (
+      (game.player1 === player && game.player1Won) ||
+      (game.player1 !== player && !game.player1Won)
+    );
   }).length;
-  console.log(wins)
+  console.log(wins);
   let losses = games.length - wins;
   res.send({ games, wins, losses });
 });
@@ -74,41 +78,50 @@ router.get('/games/:player', auth, async (req, res) => {
 // Update rating changes and winner after completion of the game
 router.patch('/games/:id', auth, async (req, res) => {
   let player1Won = req.body.player1Won;
-  console.log('Game Patch Received')
-  console.log(req.body)
-  console.log(req.params)
-  let game = await Game.findById(
-    req.params.id,
-  );
+  console.log('Game Patch Received');
+  console.log(req.body);
+  console.log(req.params);
+  let game = await Game.findById(req.params.id);
 
-  console.log(game)
+  console.log(game);
 
   let player1 = await User.findByLoginID(game.player1);
   let player2 = await User.findByLoginID(game.player2);
-
 
   let oldRatingPlayer1 = game.oldRatingPlayer1;
   let oldRatingPlayer2 = game.oldRatingPlayer2;
   let newRatingPlayer1;
   let newRatingPlayer2;
-  if(game.rated) {
-    if(player1Won) {
-      newRatingPlayer1 = Math.max(oldRatingPlayer1 + 5 + 15 * Math.min(oldRatingPlayer2 / (oldRatingPlayer1 + 1), 1), 0);
-      newRatingPlayer2 = Math.max(oldRatingPlayer2 - (5 + 15 * Math.min(oldRatingPlayer2 / (oldRatingPlayer1 + 1), 1)), 0);
-    } 
-    else {
-      newRatingPlayer1 = Math.max(oldRatingPlayer1 - (5 + 15 * Math.min(oldRatingPlayer1 / (oldRatingPlayer2 + 1), 1)), 0);
-      newRatingPlayer2 = Math.max(oldRatingPlayer2 + 5 + 15 * Math.min(oldRatingPlayer1 / (oldRatingPlayer2 + 1), 1), 0);
+  if (game.rated) {
+    if (player1Won) {
+      newRatingPlayer1 = Math.max(
+        oldRatingPlayer1 +
+          5 +
+          15 * Math.min(oldRatingPlayer2 / (oldRatingPlayer1 + 1), 1),
+        0
+      );
+      newRatingPlayer2 = Math.max(
+        oldRatingPlayer2 -
+          (5 + 15 * Math.min(oldRatingPlayer2 / (oldRatingPlayer1 + 1), 1)),
+        0
+      );
+    } else {
+      newRatingPlayer1 = Math.max(
+        oldRatingPlayer1 -
+          (5 + 15 * Math.min(oldRatingPlayer1 / (oldRatingPlayer2 + 1), 1)),
+        0
+      );
+      newRatingPlayer2 = Math.max(
+        oldRatingPlayer2 +
+          5 +
+          15 * Math.min(oldRatingPlayer1 / (oldRatingPlayer2 + 1), 1),
+        0
+      );
     }
-    player1.ratings.push({ rating: newRatingPlayer1, time: new Date()});
-    player2.ratings.push({ rating: newRatingPlayer2, time: new Date()});
-    
-    console.log(player1)
-    console.log(player2)
+
     player1.save();
     player2.save();
-  }
-  else {
+  } else {
     newRatingPlayer1 = oldRatingPlayer1;
     newRatingPlayer2 = oldRatingPlayer2;
   }
